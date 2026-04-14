d0e9cae70767cca2cd9d30db3b44d6d9b3eac877
milhar leiva <milhar.leiva935@gmail.com>
2026-04-14 16:42:58 -0400

    fix: simplify prisma config path

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,10 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
-  schema: join(__dirname, "prisma/schema.prisma"),
+  schema: "./prisma/schema.prisma",
   migrations: {
-    path: join(__dirname, "prisma/migrations"),
+    path: "./prisma/migrations",
   },
   datasource: {
     url: process.env["DIRECT_URL"],
