ee761a233f37f9e75d5783e04d05f433eb199030
milhar leiva <milhar.leiva935@gmail.com>
2026-04-15 09:58:08 -0400

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/lib/server";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   try {
     const supabase = await createClient();
