57efa898cfad467a224a8537d40f788bd9028358
milhar leiva <milhar.leiva935@gmail.com>
2026-04-15 09:30:35 -0400

    deploy nuevo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { redirect } from "next/navigation";
 import { createClient } from "@/lib/server";
 
-import { redirect } from "next/navigation";
-import { createClient } from "@/lib/server";
-
 export default async function Home() {
   const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
